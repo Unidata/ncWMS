02dcd9160672c0d307d7b79750bc162d9c44de95
mhermida <mhermida@unidata.ucar.edu>
2013-05-24 10:46:45 -0600

    Fixed bug in CdmUtils

--- src/java/uk/ac/rdg/resc/edal/cdm/CdmUtils.java
+++ src/java/uk/ac/rdg/resc/edal/cdm/CdmUtils.java
@@ -397,7 +397,7 @@ public final class CdmUtils
             //for (Date date : timeAxis.getTimeDates())
             for (CalendarDate date : timeAxis.getCalendarDates()  )
             {
-                timesteps.add(new DateTime(date, DateTimeZone.UTC));
+                timesteps.add(new DateTime(date.getMillis(), DateTimeZone.UTC));
             }
             return timesteps;
         }
